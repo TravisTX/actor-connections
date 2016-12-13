ab5ba97274cb4720891661e9ec8e82dab17f389f
Travis Collins <travistx@gmail.com>
2016-12-12 22:30:12 -0500

    debounce

--- src/app/movie-selector/movie-selector.component.ts
+++ src/app/movie-selector/movie-selector.component.ts
@@ -1,5 +1,8 @@
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/debounceTime';
 import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { MoviedbService } from '../moviedb.service';
+
 
 @Component({
   selector: 'movie-selector',
@@ -7,29 +10,37 @@ import { MoviedbService } from '../moviedb.service';
   styleUrls: ['./movie-selector.component.css']
 })
 export class MovieSelectorComponent implements OnInit {
-
   constructor(private moviedbService: MoviedbService) { }
+
   @Input()
   selectedItem = undefined;
   @Output()
   onSelected = new EventEmitter<boolean>();
   query = '';
+  queryChanged: Subject<string> = new Subject<string>();
+
   movieSearchResults = [];
   tvSearchResults = [];
 
   ngOnInit() {
+    this.queryChanged
+      .debounceTime(300) // wait 300ms after the last event before emitting last event
+      .subscribe(model => {
+        this.query = model;
+        this.searchChange(this.query)
+      });
   }
 
-  searchChange(event): void {
-    if (event.length === 0) {
+  searchChange(text): void {
+    if (text.length === 0) {
       return;
     }
-    this.moviedbService.searchForMovie(event).subscribe(
+    this.moviedbService.searchForMovie(text).subscribe(
       data => {
         this.movieSearchResults = data;
       }
     )
-    this.moviedbService.searchForTv(event).subscribe(
+    this.moviedbService.searchForTv(text).subscribe(
       data => {
         this.tvSearchResults = data;
       }
